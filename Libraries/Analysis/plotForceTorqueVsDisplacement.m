aee7feabc3ad7bea36a865e7e76f11ee89a32cb9
Joel Newbolt <joel_newbolt@brown.edu>
2023-06-15 20:30:09 -0400

    +Added trajectory arrows to torque vs. angle loop plot

--- Libraries/Analysis/plotForceTorqueVsDisplacement.m
+++ Libraries/Analysis/plotForceTorqueVsDisplacement.m
@@ -7,22 +7,20 @@ hold on
 grid on
 plot(heave_star_measured,liftcoef,'DisplayName','{\it C}_L vs. {\it y/d}','Color','red','LineWidth',4)
 plot(pitch_measured,torquezcoef,'DisplayName','{\it C}_{z\tau} vs. {\it \theta/\pi}','Color',"#FF00FF",'LineWidth',4)
+% Show arrows to indicate the direction of the force trajectory around the force loop with time
 quiver(heave_star_measured(1),liftcoef(1),heave_star_measured(2)-heave_star_measured(1),liftcoef(2)-liftcoef(1),...
     'LineWidth',4,'MaxHeadSize',1,'Color','black','AutoScaleFactor',50,'HandleVisibility','off')
 halfPeriodStep = round(1/(2*freq*T));
 quiver(heave_star_measured(halfPeriodStep),liftcoef(halfPeriodStep),...
     heave_star_measured(halfPeriodStep+1)-heave_star_measured(halfPeriodStep),liftcoef(halfPeriodStep+1)-liftcoef(halfPeriodStep),...
     'LineWidth',4,'MaxHeadSize',1,'Color','black','AutoScaleFactor',50,'HandleVisibility','off')%
-% plot(time_star,zeros(1,length(time_star)),'HandleVisibility','off','Color','#333333','LineWidth',3)
-% plot(time_star,pitch_measured,'DisplayName','{\it \theta/\pi } ','Color','black','LineWidth',4,'LineStyle','--')
-% plot(time_star,heave_star_measured,'DisplayName','{\it y/d}','Color','black','LineWidth',4)
-% plot(time_star,liftcoef,'DisplayName','{\it C}_L','Color','red','LineWidth',4)
-% plot(time_star,dragcoef,'DisplayName','{\it C}_D','Color','blue','LineWidth',4)
-% plot(time_star,torqueliftcoef,'DisplayName','{\it C}_{L\tau}','Color',"#A2142F",'LineWidth',4)
-% plot(time_star,torquedragcoef,'DisplayName','{\it C}_{D\tau}','Color',"#0072BD",'LineWidth',4)
-% plot(time_star,torquezcoef,'DisplayName','{\it C}_{z\tau}','Color',"#FF00FF",'LineWidth',4)
-% plot(time_star,dragtorquecoef,'DisplayName','{\it C}_{\tau D}','Color','green','LineWidth',4)
-% plot(time_star,powercoef,'DisplayName','C_P','Color','magenta')
+% Show arrows to indicate the direction of the torque trajectory around the force loop with time
+quiver(pitch_measured(1),torquezcoef(1),pitch_measured(2)-pitch_measured(1),torquezcoef(2)-torquezcoef(1),...
+    'LineWidth',4,'MaxHeadSize',1,'Color','black','AutoScaleFactor',50,'HandleVisibility','off')
+halfPeriodStep = round(1/(2*freq*T));
+quiver(pitch_measured(halfPeriodStep),torquezcoef(halfPeriodStep),...
+    pitch_measured(halfPeriodStep+1)-pitch_measured(halfPeriodStep),torquezcoef(halfPeriodStep+1)-torquezcoef(halfPeriodStep),...
+    'LineWidth',4,'MaxHeadSize',1,'Color','black','AutoScaleFactor',50,'HandleVisibility','off')%
 hold off
 
 legend()
