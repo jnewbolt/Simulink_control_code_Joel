1a52754aab80f7cfc44d0a987cb088f15dd5ddc9
Joel Newbolt <joel_newbolt@brown.edu>
2023-10-06 15:14:08 -0400

    +Fixed plot_profiles slowing everything down

--- Libraries/setup/plot_profiles.m
+++ Libraries/setup/plot_profiles.m
@@ -1,52 +1,31 @@
 %% plot profiles
 
-function plot_profiles(rampTimes,arg1, arg2, arg3, arg4, arg5)
+function plot_profiles(timeVector,pitchDegW,heaveMetersW, pitchDegG, heaveMetersG, refSig)
 
-    if size(arg1,2) == 5
-    % trajectories all in one array including reference signal
-        data_p1 = arg1(:,1);
-        data_h1 = arg1(:,2);
-        data_p2 = arg1(:,3);
-        data_h2 = arg1(:,4);
-        data_ref = arg1(:,5);
-    elseif size(arg1,2) == 4
-    % trajectories all in one array excluding reference signal
-        data_p1 = arg1(:,1);
-        data_h1 = arg1(:,2);
-        data_p2 = arg1(:,3);
-        data_h2 = arg1(:,4);
-        data_ref = 0;
-    else
-    % trajectories in differente individual vectors
-        data_p1 = arg1;
-        data_h1 = arg2;
-        data_p2 = arg3;
-        data_h2 = arg4;
-        if ~exist('arg5', 'var')
-            data_ref = 0;
-        else
-        data_ref = arg5;
-        end
-    end
-    
     figure
     subplot(2,1,1)
-    plot(rampTimes,data_p1); hold on;
-    plot(rampTimes,data_p2);
-    plot(rampTimes,data_ref*270,'r--'); hold off;
+    plot(timeVector,pitchDegW,"DisplayName","Pitch 1"); hold on;
+    plot(timeVector,pitchDegG,"DisplayName","Pitch 2");
+    if exist('data_ref', 'var')
+        plot(timeVector,refSig*270,'r--',"DisplayName","Reference");
+    end
+    hold off;
     ylim([-90,270])
     xlabel('time (seconds)')
     ylabel('degrees')
-    legend('pitch 1', 'pitch 2', 'ref')
+    legend()
 
     subplot(2,1,2)
-    plot(rampTimes,data_h1); hold on;
-    plot(rampTimes,data_h2);
-    plot(rampTimes,data_ref,'r--'); hold off;
+    plot(timeVector,heaveMetersW,"DisplayName","Heave 1"); hold on;
+    plot(timeVector,heaveMetersG,"DisplayName","Heave 2");
+    if exist('data_ref', 'var')
+        plot(timeVector,refSig,'r--',"DisplayName","Reference");
+    end
+    hold off;
     ylim([-0.15,0.4])
     xlabel('time (seconds)')
     ylabel('meters')
-    legend('heave 1', 'heave 2', 'ref')
+    legend()
 
     sgtitle('Traverse trajectories')
 
